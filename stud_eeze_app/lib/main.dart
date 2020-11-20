c4d76da4d6e14cb808e57debb6addda3799ff30a
savagecarol <karthiksharma1411@gmail.com>
2020-11-20 20:14:34 +0530

    main.dart added

---
+++ stud_eeze_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:provider/provider.dart';
+import 'package:stud_eeze_app/presentation/splashpage.dart';
+void main() => runApp(MultiVendor());
+
+class MultiVendor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MultiProvider(
+            providers: [
+ 
+      ],
+
+          child: MaterialApp(
+        title: "Stud Eeze",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+        ),
+        initialRoute: SplashPage.routeNamed,
+        routes: {
+           SplashPage.routeNamed: (BuildContext context) => SplashPage(),         
+        },
+      ),
+    );
+  }
+}
