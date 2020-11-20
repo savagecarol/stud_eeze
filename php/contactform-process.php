9f40a691124839d432489274f05e70a41ad1aea4
NehaAnand28 <neha280103@gmail.com>
2020-11-20 22:20:25 +0530

    contact form changed

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "jayashree1510@gmail.com";
+$EmailTo = "neha280103@gmail.com";
 $Subject = "New message from STUD EEZE landing page";
 
 // prepare email body text
