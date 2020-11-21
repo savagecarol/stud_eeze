804558acc8fd0d372bc1647a945ffd97321d4a56
savagecarol <karthiksharma1411@gmail.com>
2020-11-21 18:53:03 +0530

    pdf view addded

--- stud_eeze_app/lib/presentation/tab_pages/post_page.dart
+++ stud_eeze_app/lib/presentation/tab_pages/post_page.dart
@@ -1,17 +1,45 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+
 
 class PostPage extends StatefulWidget {
-  PostPage({Key key}) : super(key: key);
-
   @override
   _PostPageState createState() => _PostPageState();
 }
 
 class _PostPageState extends State<PostPage> {
+  String url = "http://www.pdf995.com/samples/pdf.pdf";
+  String pdfasset = "assets/sample.pdf";
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() { 
+    super.initState();
+    _initPdf();
+  }
+
+  _initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromURL(url);
+    setState(() {
+      _doc=doc;
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       
+    return Scaffold(
+      appBar: AppBar(title: Text("My Documnet"),),
+      body: _loading ? Center(child: CircularProgressIndicator(),) : PDFViewer(document: _doc, 
+      indicatorBackground: Colors.red,
+      // showIndicator: false,
+      // showPicker: false,
+       ),
     );
   }
 }
